8ca7eafe6301a9032827c951eb1deb7d471e02c4
Takumi Sugimoto <ts20030211@gmail.com>
2026-07-25 17:40:25 +0900

    fix memory leak and ensure unity 6.3 compability

--- Assets/SkinnedMeshFlow/SkinnedMeshFlow.cs
+++ Assets/SkinnedMeshFlow/SkinnedMeshFlow.cs
@@ -1,20 +1,21 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.VFX;
 using System.Collections.Generic;
 
 public class SkinnedMeshFlow : MonoBehaviour
 {
     public ComputeShader shader;
     public SkinnedMeshRenderer skinnedMeshRenderer;
+    public VisualEffect visudalEffect;
 
-    private RenderGraph _renderGraph;
     private SkinnedMeshFlowPass _pass;
 
     void OnEnable()
     {
-        _renderGraph = new RenderGraph("Skinned Mesh Flow Graph");
-        _pass = new SkinnedMeshFlowPass(shader, skinnedMeshRenderer);
+        _pass = new SkinnedMeshFlowPass(shader, skinnedMeshRenderer, visudalEffect);
 
         RenderPipelineManager.beginCameraRendering += OnBeginCamera;
     }
@@ -28,29 +29,7 @@ public class SkinnedMeshFlow : MonoBehaviour
 
     private void OnBeginCamera(ScriptableRenderContext ctx, Camera cam)
     {
-        var cmd = CommandBufferPool.Get("Skinned Mesh Flow Command");
-        var param = new RenderGraphParameters
-        {
-            scriptableRenderContext = ctx,
-            commandBuffer = cmd,
-            currentFrameIndex = Time.frameCount,
-        };
-
-        try
-        {
-            _renderGraph.BeginRecording(param);
-            _pass.RecordRenderGraph(_renderGraph);
-            _renderGraph.EndRecordingAndExecute();
-        }
-        catch (System.Exception e)
-        {
-            _renderGraph.Cleanup();
-            throw e;
-        }
-
-        ctx.ExecuteCommandBuffer(cmd);
-        CommandBufferPool.Release(cmd);
-        ctx.Submit();
+        cam.GetUniversalAdditionalCameraData().scriptableRenderer.EnqueuePass(_pass);
     }
 }
 
@@ -65,12 +44,16 @@ class PassData
     public BufferHandle adjacentBuffer;
     public BufferHandle addressBuffer;
     public BufferHandle strainBuffer;
+
+    public GraphicsBuffer srcVertexBufferResource;
+    public GraphicsBuffer dstVertexBufferResource;
 }
 
-class SkinnedMeshFlowPass : System.IDisposable
+class SkinnedMeshFlowPass : ScriptableRenderPass
 {
     private ComputeShader _shader;
     private SkinnedMeshRenderer _skinnedMeshRenderer;
+    private VisualEffect _visudalEffect;
 
     private int _vertexCount;
 
@@ -78,13 +61,17 @@ class SkinnedMeshFlowPass : System.IDisposable
     private GraphicsBuffer _addressBuffer;
     private GraphicsBuffer _strainBuffer;
 
-    public SkinnedMeshFlowPass(ComputeShader shader, SkinnedMeshRenderer skinnedMeshRenderer)
+    private MaterialPropertyBlock _propertyBlock;
+
+    public SkinnedMeshFlowPass(ComputeShader shader, SkinnedMeshRenderer skinnedMeshRenderer, VisualEffect visudalEffect)
     {
         if (shader == null) throw new System.ArgumentNullException(nameof(shader));
         if (skinnedMeshRenderer == null) throw new System.ArgumentNullException(nameof(skinnedMeshRenderer));
+        if (visudalEffect == null) throw new System.ArgumentNullException(nameof(visudalEffect));
 
         _shader = shader;
         _skinnedMeshRenderer = skinnedMeshRenderer;
+        _visudalEffect = visudalEffect;
 
         _skinnedMeshRenderer.sharedMesh.vertexBufferTarget |= GraphicsBuffer.Target.Structured;
         _skinnedMeshRenderer.vertexBufferTarget |= GraphicsBuffer.Target.Structured;
@@ -133,16 +120,17 @@ class SkinnedMeshFlowPass : System.IDisposable
         _addressBuffer.SetData(neighbors);
 
         _strainBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, _vertexCount, 4 * 3);
+
+        _propertyBlock = new MaterialPropertyBlock();
     }
 
-    public void RecordRenderGraph(RenderGraph renderGraph)
+    public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
     {
         var srcVertexBuffer = _skinnedMeshRenderer.sharedMesh.GetVertexBuffer(0);
         var dstVertexBuffer = _skinnedMeshRenderer.GetVertexBuffer();
 
-        if (dstVertexBuffer == null) return;
-
-        var adjustMatrix = _skinnedMeshRenderer.worldToLocalMatrix * _skinnedMeshRenderer.rootBone.localToWorldMatrix;
+        if (srcVertexBuffer == null || dstVertexBuffer == null) return;
+        Debug.Log("SkinnedMeshFlowPass: RecordRenderGraph");
 
         using (var builder = renderGraph.AddComputePass<PassData>("Skinned Mesh Flow Pass", out var passData))
         {
@@ -155,6 +143,14 @@ class SkinnedMeshFlowPass : System.IDisposable
             passData.adjacentBuffer = renderGraph.ImportBuffer(_adjacentBuffer);
             passData.addressBuffer = renderGraph.ImportBuffer(_addressBuffer);
             passData.strainBuffer = renderGraph.ImportBuffer(_strainBuffer);
+            passData.srcVertexBufferResource = srcVertexBuffer;
+            passData.dstVertexBufferResource = dstVertexBuffer;
+
+            builder.UseBuffer(passData.srcVertexBuffer, AccessFlags.Read);
+            builder.UseBuffer(passData.dstVertexBuffer, AccessFlags.Read);
+            builder.UseBuffer(passData.adjacentBuffer, AccessFlags.Read);
+            builder.UseBuffer(passData.addressBuffer, AccessFlags.Read);
+            builder.UseBuffer(passData.strainBuffer, AccessFlags.Write);
 
             builder.SetRenderFunc(static (PassData passData, ComputeGraphContext ctx) =>
             {
@@ -168,12 +164,18 @@ class SkinnedMeshFlowPass : System.IDisposable
                 ctx.cmd.SetComputeBufferParam(passData.shader, kernelId, "AddressBuffer", passData.addressBuffer);
                 ctx.cmd.SetComputeBufferParam(passData.shader, kernelId, "StrainBuffer", passData.strainBuffer);
                 ctx.cmd.DispatchCompute(passData.shader, kernelId, groupX, 1, 1);
+
+                passData.srcVertexBufferResource.Dispose();
+                passData.dstVertexBufferResource.Dispose();
             });
         }
 
-        var block = new MaterialPropertyBlock();
-        block.SetBuffer("StrainBuffer", _strainBuffer);
-        _skinnedMeshRenderer.SetPropertyBlock(block);
+        _skinnedMeshRenderer.GetPropertyBlock(_propertyBlock);
+        _propertyBlock.SetBuffer("StrainBuffer", _strainBuffer);
+        _skinnedMeshRenderer.SetPropertyBlock(_propertyBlock);
+
+        // DEBUG
+        _visudalEffect.SetGraphicsBuffer("StrainBuffer", _strainBuffer);
     }
 
     public void Dispose()
